d02f4b51a529f5166e563ba65b8975d89587f7bc
Anthony Moss <asmoss93@gmail.com>
2019-04-01 11:27:23 -0400

    adds express way to route methods

--- index.js
+++ index.js
@@ -1,102 +1,39 @@
+const express = require('express'); // Bring in express library
+const app = express(); // Create a new express app
+
 const http = require('http');
 const querystring = require('querystring');
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
+// Import my model class
 const Restaurant = require('./models/restaurants');
-const Users = require('./models/user');
+const User = require('./models/user');
 const Reviews = require('./models/reviews')
 
-
-
-// "helper function" === 'middleware'
-// aka "request handler"
-const server = http.createServer(async (req, res) => {
-    const method = req.method;
-    const parts = req.url.split("/");
-    console.log(req);
-
-    res.statusCpde = 200;
-    res.setHeader('Content-Type', 'application/json');
-
-    // if req.url is "/restaurants", send them all restaurants
-    //if it's "/user", send a list of users
-    //else if it doesnt match either, send a welcome message
-    if (req.url === "/restaurants") {
-        // const method = req.method;
-        // const parts = req.url.split("/");
-        console.log(req);
-        if (method === "GET") {
-            if (parts.length === 2) {
-                const allRestaurants = await Restaurant.getAll();
-                const restaurantJSON = JSON.stringify(allRestaurants);
-                res.end(restaurantJSON);
-            } else if (parts.length === 3) {
-                const restaurantId = parts[2];
-                const theRestaurant = await Users.getById(restaurantId);
-                const restaurantJSON = JSON.stringify(theRestaurant);
-                res.end(restaurantJSON);
-            } else {
-                res.status = 404;
-                res.end('Resource not found.');
-            } 
-        } else if (method === "POST") {
-            res.end('{ message: "it sounds like you wanna create"}');
-        } else if (method === "PUT") {
-            res.end('{ message: "you wanna update, dont you"}');
-        } else if (method === "DELETE") {
-            res.end('{ message: "rm the user"}');
-        }
-    } else if (req.url.startsWith ("/users")) {
-        if (method === "GET") {
-            if (parts.length === 2) {
-                const allUsers = await User.getAll();
-                const userJSON = JSON.stringify(allUsers);    
-                res.end(userJSON);
-            } else if (parts.length === 3) {
-                // the id will be parts[2]
-                const userId = parts[2];
-                // get user by id
-                const theUser = await User.getById(userId);
-                const userJSON = JSON.stringify(theUser);
-                res.end(userJSON);
-            } else {
-                res.statusCode = 404;
-                res.end('Resource not found.');
-            }
-        } else if (method === "POST") {
-            // let's read those chunks!
-            let body = '';
-            req.on('data', (chunk) => {
-                // .toString() is built into most objects
-                // it returns a string representation of the object
-                body += chunk.toString();
-            });
-
-            req.on('end', async () => {
-                const parsedBody = querystring.parse(body);
-                console.log('====================');
-                console.log(parsedBody);
-                console.log('^^^^^^ BODY OF FORM ^^^^^^^^');
-                const newUserId = await User.add(parsedBody);
-                res.end(`{ "id": ${newUserId}}`);
-            });
-
-
-        } else if (method === "PUT") {
-            res.end('{ "message": "you wanna update, doncha?"}');
-        } else if (method === "DELETE") {
-            res.end('{ "message": "rm the user"}');
-        }
-
-    } else {
-        res.end(`{
-            message: "Thank you for your patronage. Please send bitcoin."
-        }`);
-    }
+app.get('/restaurants', async (req, res) => {
+    const allRestaurants = await Restaurant.getAll();
+    // const restaurantJSON = JSON.stringify(allRestaurants);
+    // res.json will do 2 things:
+    // 1. it converts your javaScript Object or Array to a JSON String
+    // 2. it puts the correct Content-Type header on the response
+    res.json(allRestaurants);
 });
 
-server.listen(port, hostname, () => {
-    console.log(`Server is running at http://${hostname}:${port}`);
+app.get('/users', async (req, res) => {
+    const allUsers = await User.getAll();  
+    res.json(allUsers);
+});
+app.get('/users/:id', async (req, res) =>{
+    // how to grab a piece out of req.params (or any object):
+    // const id = req.params.id;
+    // this is known as "destructuring"
+    const {id} = req.params;
+    const theUser =  await User.getById(req.params.id);
+    res.json(theUser);
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
