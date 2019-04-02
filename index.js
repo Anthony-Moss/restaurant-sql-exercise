bd4902068880ea9d61dfa2f93b9f42fc12f1b095
Anthony Moss <asmoss93@gmail.com>
2019-04-02 09:28:21 -0400

    replaced handler functions by using them from controller.js

--- index.js
+++ index.js
@@ -8,10 +8,12 @@ const querystring = require('querystring');
 const hostname = '127.0.0.1';
 const port = 3000;
 
+
+
 // Import my model class
 const Restaurant = require('./models/restaurants');
-const User = require('./models/user');
-const Reviews = require('./models/reviews')
+const Reviews = require('./models/reviews');
+const controller = require('./models/controller')
 
 app.get('/restaurants', async (req, res) => {
     const allRestaurants = await Restaurant.getAll();
@@ -22,32 +24,43 @@ app.get('/restaurants', async (req, res) => {
     res.json(allRestaurants);
 });
 
-app.get('/users', async (req, res) => {
-    const allUsers = await User.getAll();  
-    res.json(allUsers);
-});
-app.get('/users/:id', async (req, res) => {
-    // how to grab a piece out of req.params (or any object):
-    // const id = req.params.id;
-    // this is known as "destructuring"
-    const {id} = req.params;
-    const theUser =  await User.getById(req.params.id);
-    res.json(theUser);
-});
-app.post('/users', async (req, res) => {
-    const newUser = await User.add(req.body);
-    res.json(newUser);
-});
+app.get('/users', controller.log, controller.checkAllUsers);
 
-app.put('/users/:id', async (req, res) => {
-    const updatedInfo = await User.update(req.params.id, req.body);
-    res.json(updatedInfo);    
-});
+app.get('/users/:id', controller.checkUserInfo);
 
-app.delete('/users/:id', async (req, res) => {
-    let userInfo = await User.deleteById(req.params.id);
-    res.json(userInfo);
-})
+app.post('/users', controller.createNewUser);
+
+app.put('/users/:id', controller.updateUser);
+
+app.delete('/users/:id', controller.deleteUser);
+
+
+// app.get('/users', async (req, res) => {
+//     const allUsers = await User.getAll();  
+//     res.json(allUsers);
+// });
+// app.get('/users/:id', async (req, res) => {
+//     // how to grab a piece out of req.params (or any object):
+//     // const id = req.params.id;
+//     // this is known as "destructuring"
+//     const {id} = req.params;
+//     const theUser =  await User.getById(req.params.id);
+//     res.json(theUser);
+// });
+// app.post('/users', async (req, res) => {
+//     const newUser = await User.add(req.body);
+//     res.json(newUser);
+// });
+
+// app.put('/users/:id', async (req, res) => {
+//     const updatedInfo = await User.update(req.params.id, req.body);
+//     res.json(updatedInfo);    
+// });
+
+// app.delete('/users/:id', async (req, res) => {
+//     let userInfo = await User.deleteById(req.params.id);
+//     res.json(userInfo);
+// })
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
